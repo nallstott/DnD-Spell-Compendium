a452752a6deaa7b39ce23247785c91e98122ddc3
nallstott <nallstott@gmail.com>
2020-04-23 18:59:59 -0700

    Update search.js

--- search.js
+++ search.js
@@ -1,5 +1,5 @@
 import * as spells from './spellsModule.js';
-console.log(spells.fireball.name)
+console.log(spells.fireball)
 // const searchBtn = document.querySelector('#searchBtn');
 // const searchedSpellList = document.querySelector('.searched-spell-list')
 // let searchedSpell = document.querySelector('.list-input')
