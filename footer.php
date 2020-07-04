62465d8ea5d3b282fa1e27ad1edec3e2f0524432
nallstott <nallstott@gmail.com>
2020-07-04 18:39:57 -0400

    pre PHP overhaul commit
    Committing before transitioning header and footer elements to PHP

---
+++ footer.php
@@ -0,0 +1,6 @@
+<?php
+    echo '
+        <footer>
+            <p class="disclaimers">The DnD 5e Spell Compendium is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.</p>
+        </footer>
+    ';
---
+++ header.php
@@ -0,0 +1,13 @@
+<?php
+    echo '
+    <nav>
+		<div class="nav-left">
+			<h4 class="nav-item" id="home-trigger"><a href="index.html">DnD 5e Spell Compendium</a></h4>
+		</div>
+		<div class="nav-right">
+			<h4 class="nav-item" id="search-trigger"><a href="search.html">Spell Search</a></h4>
+			<h4 class="nav-item" id="classes-menu"><a href="index.html#cleric-page-link-div">Classes</a></h4>
+		</div>
+	</nav>
+    ';
+?>
